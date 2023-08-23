3f7a5c0c5d5a3942cd39fd0dcde98940d91cd13c
cafferti <saulkumven@gmail.com>
2023-08-23 13:09:41 +0100

    started api exercise

--- api.js
+++ api.js
@@ -3,9 +3,16 @@
 const getAllUserEmail = async () =>{
     const response = await fetch(`https:jsonplaceholder.typicode.com/users`);
     jsonuserdata = await response.json();
-    console.log(jsonuserdata)
+   
+    // jsonuserdata.forEach(user => {
+    //     console.log(user.email)
+    // }); only when you want to display the list of emails
+    //but the below makes map craetes another array and stores emails as strings
 
-// const useremailArray = jsonuserdata.map(()=>{})
+   const useremailArray = jsonuserdata.map((user)=>{
+    return user.email
+   })
+//    console.log(useremailArray)
 }
 
 getAllUserEmail()
